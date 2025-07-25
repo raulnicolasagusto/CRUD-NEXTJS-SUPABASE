071277aff4f5b447654a6492eee46214b22a1ed2
Raul Agusto <raulnicolasagusto@gmail.com>
2025-07-25 19:11:24 -0300

    practicando CRUD supabase NEXT JS

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,157 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
+import { useEffect, useState } from "react";
+import { supabase } from "@/lib/supabase";
+import toast, { Toaster } from "react-hot-toast";
+import Swal from "sweetalert2";
+import "sweetalert2/dist/sweetalert2.min.css";
+import { ifError } from "assert";
+
+interface Student{
+  id?:string,
+  name:string,
+  email:string,
+  phone_numb:string,
+  gender:string
+}
+
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  const [students, setStudents] = useState<Student[]>([])
+  const [form, setForm] = useState<Student>({
+    name:"",
+    email:"",
+    phone_numb:"",
+    gender:"Male"
+  })
+
+  const [editId, setEditId] = useState<string | null>(null)
+
+  useEffect( ()=>{
+   fetchStudents(); 
+  }, [])
+
+  // hablde form submit
+  async function handleFormSubmit(event: FormEvent<HTMLFormElement>){
+    event.preventDefault();
+    console.log(form)
+
+    //aca metemos los datos del form en la base de datos supabase
+   const {error} = await supabase.from<Student>("students").insert([form])
+
+    if(error){
+      toast.error('Error al crear ${error.message}')
+      console.log(error.message);
+    }else{
+      toast.success("Agregado exitosamente.")
+    }
+
+    setForm({
+      name:"",
+     email:"",
+      phone_numb:"",
+      gender:"Male"
+    })
+  }
+
+  //Con esta funcion mostramos los estudiantes. Agregamos arriga el useeffect con el fetchStudents()
+  async function fetchStudents(){
+    const { error, data } = await supabase.from("students").select("*");
+
+    if (error) {
+      toast.error("Error al mostrar los datos")
+      console.log(error.message)
+      
+    } else {
+      setStudents(data || [])
+      console.log(data)
+      
+    }
+  }
+  
+  return (
+    <>
+      <div className="container my-5">
+        <Toaster/>
+        <h3 className="mb-4">Estudiantes</h3>
+         <div className="row">
+           {/* Left Side Form */}
+            <div className="col-md-4">
+              <div className="card mb-4">
+                <div className="card-body">
+                   <form onSubmit={handleFormSubmit}>
+                    <div className="mb-3">
+                      <label className="form-label">Name</label>
+                      <input type="text" className="form-control" value={ form.name } onChange={ (event) => setForm({
+                        ...form,
+                        name: event.target.value
+                      }) }/>
+                    </div>
+                    <div className="mb-3">
+                      <label className="form-label">Email</label>
+                      <input type="email" className="form-control" value={ form.email } onChange={ (event) => setForm({
+                        ...form,
+                        email: event.target.value
+                      }) }/>
+                    </div>
+                    <div className="mb-3">
+                      <label className="form-label">Phone Number</label>
+                      <input type="text" className="form-control" value={ form.phone_numb } onChange={ (event) => setForm({
+                        ...form,
+                        phone_numb: event.target.value
+                      }) }/>
+                    </div>
+                    <div className="mb-3">
+                      <label className="form-label">Gender</label>
+                      <select className="form-select" value={ form.gender } onChange={ (event) => setForm({
+                        ...form,
+                        gender: event.target.value
+                      }) }>
+                        <option value="male">Male</option>
+                        <option value="female">Female</option>
+                        <option value="other">Other</option>
+                      </select>
+                    </div>
+                    <button className="btn btn-primary w-100">Agregar</button>
+                   </form>
+                </div>
+              </div>
+            </div>
+          {/** right side table */}
+          <div className="col-md-8">
+            <div className="table-responsive">
+              <table className="table table-bordered">
+                <thead className="table-light">
+                  <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Gender</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {
+                    students.map( (singleStudent)=>(
+                      <tr>
+                          <td>{singleStudent.name}</td>
+                          <td>{singleStudent.email}</td>
+                          <td>{singleStudent.phone_numb}</td>
+                          <td>{singleStudent.gender}</td>
+                          <td>
+                          <button className="btn btn-warning btn-sm me-2">Edit</button>
+                          <button className="btn btn-danger btn-sm me-2">Delete</button>
+                          </td>
+                  </tr>
+                    ) )
+                  }
+                  
+                </tbody>
+              </table>
+            </div>
+          </div>
+         </div>
+      </div>
+    </>
   );
 }
